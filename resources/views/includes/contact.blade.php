de490b52eae7c660eca5de6ba9ea13711b7b3aec
Mero1983 <marwa_barakat@yahoo.com>
2024-07-23 20:03:16 +0300

    send contact mail

--- resources/views/includes/contact.blade.php
+++ resources/views/includes/contact.blade.php
@@ -1,7 +1,7 @@
 <div id="contact" class="tm-page-content">
             <div class="tm-black-bg tm-contact-text-container">
               <h2 class="tm-text-primary">Contact Wave</h2>
-              <p>Wave Cafe Template has a video background. You can use this layout for your websites. Please contact Tooplate's Facebook page. Tell your friends about our website.</p>
+              <p>{Wave Cafe Template has a video background. You can use this layout for your websites. Please contact Tooplate's Facebook page. Tell your friends about our website.}</p>
             </div>
         <form action="{{ route('contact.send') }}" method="POST" id="contact-form">
             @csrf
